dda6d76c38fd7fed85c4f2204fb5e4f09d194620
Yu Xiang <yuxiang@capri5.Stanford.EDU>
2015-10-03 21:17:12 -0700

    compile mex

--- compile.m
+++ compile.m
@@ -4,4 +4,8 @@ include = ' -I/usr/local/include/opencv/ -I/usr/local/include/';
 lib = ' -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_video';
 eval(['mex lk.cpp -O' include lib]);
 
+mex distance.cpp 
+mex imResampleMex.cpp 
+mex warp.cpp
+
 disp('Compilation finished.');
